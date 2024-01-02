71bb388112d1dc78153e8a55ba27dd0b8338ea37
Prajwol Gautam <prajwolgautam@pg-2.local>
2024-01-02 23:26:25 +0530

    first commit

---
+++ src/components/Project.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import ProjectSection from "./ProjectSection";
+AOS.init();
+
+const Project = () => {
+  return (
+    <section className=" dotted py-2">
+    <br></br><br></br>
+      <div className="container mx-auto px-4">
+
+
+
+
+       <div
+        data-aos=" justify zoom-in"
+        className="   text-5xl font-extrabold   h-40"
+      >
+        <div className="title  font-bold text-xl  tracking-wide  float-right">
+          <h1 className="float-right text-right  text-amber-500	"> Hightlight Projects</h1>{" "}
+        </div><br></br>  
+        <p className=" font-sans	 Montserrat text-2xl  text-gray-700   font-medium	  float-right text-right">
+        Some Of the  
+        <span className="text-[#5DB85E]  " > Key </span>
+      Hightlight Projects   <br></br> done by EERC team in different fields.  
+      </p>
+
+ 
+
+
+
+      </div>
+
+
+        {/* <h2 className="text-right text-3xl font-semibold text-gray-800 mb-8">
+          Our Projects
+        </h2>
+        <p className=" font-sans	 Montserrat text-2xl  text-gray-700   font-medium	 mx-auto">
+      <span className="text-[#5DB85E] text-right " >EERC </span>  
+      is a consulting and research institute registered in 6th May 2016 in
+       the Office of Company Registrar at Tripureshwor, Kathmandu.  
+      </p> */}
+        {/* Inside ProjectSection.js */}
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2  gap-8">
+          <ProjectSection/>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Project;
---
+++ src/components/ServicesDetail.jsx
@@ -0,0 +1,61 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
+import services from "../API/services.json";
+function ServicesDetail() {
+  return (
+    <>
+    <hr></hr>
+    <section className="dotted  px-8">
+
+      {/* Heading section for Services */}
+      <div
+        data-aos=" justify zoom-in"
+        className="dotted mx-1 text-5xl font-extrabold   h-60"
+      >
+        <div className="title py-4 font-extrabold text-2xl  tracking-wide ">
+          <h1 className="text-left text-amber-500	"> Our Services</h1>{" "}
+        </div>
+        <p className=" font-sans	 Montserrat text-3xl  text-gray-700   font-medium	 mx-auto">
+      <span className="text-[#5DB85E] " >EERC </span>  
+      is a consulting and research institute registered in  <br></br> 6th May 2016
+       the Office of Company Registrar Kathmandu.  
+      </p>
+      </div>
+
+      {/* Content section for rendering services from JSON  */}
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-3 gap-8">
+        {services.services.map((service) => {
+         
+          return (
+            //
+
+            <div  key={service.serviceId}className="bg-white bg-opacity-20 backdrop-blur-lg rounded-lg shadow-lg hover:shadow-xl overflow-hidden flex flex-col md:flex-row transform transition-transform hover:scale-105">
+              <div className="p-4 flex-1">
+                <h3 className="flex items-center text-xl font-semibold text-black mb-2">
+                  <i className="mr-2 fas fa-wrench"></i>
+                  {service.name}
+                </h3>
+                <p className="text-back-200">{service.description}</p>
+              </div>
+              <div className="p-4">
+                <div className="relative">
+                  <img
+                    src="../../public/images/services/1.png"
+                    alt="Service 1"
+                    className="w-20 h-20 object-cover rounded-full"
+                  />
+                </div>
+              </div>
+            </div>
+            // </div>
+          );
+        })}
+      </div>
+      </section><br></br>
+      <hr></hr>
+    </>
+  );
+}
+
+export default ServicesDetail;
