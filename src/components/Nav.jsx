4569e79dea449a04893cb04f14776dac3d9e0783
Raj Kasaudhan <rajksd9@gmail.com>
2024-01-31 23:05:47 +0530

    FIX

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,81 +1,80 @@
-import React, { useState } from "react";
-import { Button } from "flowbite-react";
+import React from "react";
+
 import { Link } from "react-router-dom";
 
 function Nav() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <nav className="flex items-center justify-between flex-wrap p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6 lg:mr-72">
-        <img
-          src="public/images/eercLogo.png"
-          className="w-100 h-10 mr-2"
-          alt="Logo"
-        />
-      </div>
-      <div className="block lg:hidden bg-white">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img
+            src="public/images/eercLogo.png"
+            className="w-100 h-10 mr-2"
+            alt="Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            EERC
+          </span>
+        </a>
         <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
         >
+          <span className="sr-only">Open main menu</span>
           <svg
-            className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
-            viewBox="0 0 20 20"
+            className="w-5 h-5"
+            aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
           >
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-          <svg
-            className={`fill-current h-3 w-3 ${isOpen ? "block" : "hidden"}`}
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
           </svg>
         </button>
-      </div>
-      <div
-        className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${
-          isOpen ? "block" : "hidden"
-        }`}
-      >
-        <div className="text-sm lg:flex-grow text-white">
-          <ul className="flex ">
+        <div className="hidden  w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <Link to="/">
-              <li className="flex-1 ">Home</li>
+              <li
+                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                aria-current="page"
+              >
+                Home
+              </li>
             </Link>
-
             <Link to="/about">
-              <li className="flex-1 mx-12">About</li>
+              <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                About
+              </li>
             </Link>
-
             <Link to="/blogs">
-              <li className="flex-1 mx-12">Blog</li>
+              <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Blog
+              </li>
             </Link>
-
             <Link to="/projects">
-              <li className="flex-1 mx-12">Projects</li>
+              <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Projects
+              </li>
             </Link>
-
             <Link to="/contact">
-              <li className="flex-1 mx-12">Contact</li>
+              <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Contact
+              </li>
             </Link>
           </ul>
-        </div>
-
-        <div className="flex justify-end ">
-          <Button gradientMonochrome="pink" className="right-0 mt-4 ml-64">
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900 right-0"
-            >
-              Contact <span aria-hidden="true">&rarr;</span>
-            </a>
-          </Button>
         </div>
       </div>
     </nav>
   );
 }
+
 export default Nav;
