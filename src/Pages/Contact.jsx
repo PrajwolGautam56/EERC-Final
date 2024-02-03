6f8b74d3809729a87cf8b1e4247297793d50cfbb
Raj Kasaudhan <rajksd9@gmail.com>
2024-02-04 00:53:45 +0530

    Added functionality to send messages

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import ContactPage from "../components/Contact";
+
 import Nav from "../components/Nav";
 import Contacts from "../components/Contacts";
 
